9aaf6741a70606696adbf3c69a4318eaf607bd2d
Gonzalo Moreno <gonzalo.moreno@polaris.es>
2022-10-11 18:37:46 +0200

    First Upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # UnblockPDFs
+The proposal of this project is unblock PDFs with password and remove all security of the PDF file.
+
+Then needs to follow the steps:
+1. Download the Zip for you OS version
+2. Execute the file UnblockPDFs.exe
+3. File path + Enter
+4. File password + Enter
+5. File Unblocked on the same path that file, with end _unblocked.pdf
+
+##License
+MIT License
---
+++ UnblockPDFs/Program.cs
@@ -0,0 +1,31 @@
+﻿using UglyToad.PdfPig;
+using UglyToad.PdfPig.Writer;
+
+Console.WriteLine("Path to PDF File:");
+string filePath = Console.ReadLine();
+Console.WriteLine("Password of PDF File:");
+string filePassword = Console.ReadLine();
+
+try
+{
+    using (PdfDocument document = PdfDocument.Open(filePath, new ParsingOptions { Password = filePassword }))
+    {
+        PdfDocumentBuilder builder = new PdfDocumentBuilder();
+        var pages = document.GetPages();
+        var pagecounts = pages.Count();
+        for (int i = 1; i <= pagecounts; i++)
+        {
+            builder.AddPage(document, i);
+        }
+        File.WriteAllBytes(filePath.Replace(".pdf", "_unblock.pdf"), builder.Build());
+        Console.WriteLine("PDF Saved: " + filePath.Replace(".pdf", "_unblock.pdf"));
+        Console.ReadLine();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("ERROR: " + ex.Message + " " + ex.StackTrace);
+    Console.ReadLine();
+}
+
+
